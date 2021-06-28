c7bef7d275d8b235cc410bb59f4d67f83ee51697
David Gubala <gubaladev@gmail.com>
2021-06-28 11:15:26 -0500

    first commit

---
+++ fea_0/src/App.tsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import './App.css';
+
+function App () {
+    return(
+        /*  This is the react component where we put the html.
+            This object is the app itself so it will load in all
+            necesarry components
+            suchas :
+                <todoInput />
+                <todoList />
+        */
+       <div className="App">
+       </div>
+    );
+}
+
